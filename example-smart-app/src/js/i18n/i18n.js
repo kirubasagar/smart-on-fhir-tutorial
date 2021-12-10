a1c6dea7eeca91c816ee123c697f1bd82b817ec3
Patel <KP027173@cerner.net>
2021-12-10 12:44:25 -0600

    Added i18n

---
+++ example-smart-app/src/js/i18n/i18n.js
@@ -0,0 +1,11 @@
+// If the i18n object doesn't already exist, create one.
+if(typeof i18n === 'undefined') {
+    var i18n = {};
+}
+
+// Add some of our i18n strings to the object.
+i18n.Immunization = {
+    BIRTHDATE: 'Birth Date',
+    NAME: 'Name'
+
+}
