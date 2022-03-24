2de2f0760e7c89e58fdacfab75a8fc02be1a6deb
kirubasagar <53877031+kirubasagar@users.noreply.github.com>
2022-03-24 14:04:35 +0530

    test

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -10,7 +10,7 @@
 
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
-        alert("test");
+        alert("testLatest");
 
         var patientId = null;
         if (smart.tokenResponse) {
