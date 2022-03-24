a34b978c77f6ba931a8651008d1e9365e58f168a
kirubasagar <53877031+kirubasagar@users.noreply.github.com>
2022-03-24 10:43:31 +0530

    Create i18n.js

---
+++ example-smart-app/src/js/i18n/i18n.js
@@ -0,0 +1,13 @@
+// If the i18n object doesn't already exist, create one.
+if(typeof i18n === 'undefined') {
+    var i18n = {};
+}
+
+// Add some of our i18n strings to the object.
+i18n.Immunization = {
+    BIRTHDATE: 'Date of Birth',
+    COVID_VACCINATION_RECORD_CARD: 'COVID-19 Vaccination Record Card',
+    NAME: 'Name',
+    HEALTH_CARD_INFOMATION_ONE: 'Use your modile device to take a screen shot of each QR code available to you. Keep each screen shot in a secure location for offline viewing'
+
+}
