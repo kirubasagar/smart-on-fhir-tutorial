529fe213e92261434b481aea9142438627d39ca9
kirubasagar <53877031+kirubasagar@users.noreply.github.com>
2022-04-08 12:39:57 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -10,7 +10,7 @@
 
     function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
-        alert("testLatest7");
+        alert("testLatest8");
 
         var patientId = null;
         var bearerToken = null;
@@ -19,7 +19,6 @@
              var encounterId = smart.tokenResponse.encounter;
              var userId = smart.tokenResponse.user;
              console.log(smart.tokenResponse);
-             bearerToken = smart.tokenResponse.access_token;
         }
 
         callHealthCardEndpoint(ret,patientId,bearerToken)
@@ -39,9 +38,9 @@ function callHealthCardEndpoint(ret,patientId,bearerToken)
   alert("innn");
   alert(patientId);
   alert("plsnew");
-  //patientId = 12724065;// 3374491 // 3213970 // 12724065 //12724069
-  //var url = "https://fhir-open.stagingcerner.com/beta/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient/" + patientId + "/$health-cards-issue";
-  var url = "https://fhir-myrecord.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient/" + patientId + "/$health-cards-issue"
+  patientId = 12724065;// 3374491 // 3213970 // 12724065 //12724069
+  var url = "https://fhir-open.stagingcerner.com/beta/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient/" + patientId + "/$health-cards-issue";
+  //var url = "https://fhir-myrecord.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Patient/" + patientId + "/$health-cards-issue"
 
   var request = new XMLHttpRequest();
   request.open("POST", url, true);
@@ -62,7 +61,6 @@ function callHealthCardEndpoint(ret,patientId,bearerToken)
   alert(bearerToken);
   request.setRequestHeader("Accept", "application/fhir+json");
   request.setRequestHeader("Content-Type", "application/fhir+json");
-  request.setRequestHeader("Authorization", "Bearer "+ bearerToken);
   var body = "{\"resourceType\":\"Parameters\","
              + "\"parameter\":["
              +  "{\"name\":\"credentialType\",\"valueUri\":\"https://smarthealth.cards#immunization\"},"
